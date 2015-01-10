2c9e5fcadecc6fdc844f37c351de778f23b5aa83
Chilledheart <rwindz0@gmail.com>
2015-01-10 11:25:05 +0800

    fix ci build

--- scripts/install-deps-linux.sh
+++ scripts/install-deps-linux.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -x
+set -e
 
-sudo add-apt-repository -y ppa:george-edison55/cmake-precise
+sudo add-apt-repository -y ppa:smspillaz/cmake-2.8.12
 sudo add-apt-repository -y ppa:beineri/opt-qt54
 sudo apt-get update -qq
-sudo apt-get install -qq valac uuid-dev libevent-dev re2c libjansson-dev cmake libqt4-dev
-sudo apt-get install -qq qt54base qt54translations qt54tools qt54webkit
+sudo apt-get install -y valac uuid-dev libevent-dev re2c libjansson-dev cmake cmake-data libqt4-dev
+sudo apt-get install -y qt54base qt54translations qt54tools qt54webkit
 
 git clone --depth=1 --branch=master git://github.com/haiwen/libsearpc.git deps/libsearpc
 git clone --depth=1 --branch=master git://github.com/haiwen/ccnet.git deps/ccnet
